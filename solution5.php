d755be001861cf7a5a605917e90adc5c97028085
trentraynard <trentersr@gmail.com>
2019-07-03 15:59:38 -0700

    solution 5 half way done(solved but needs improvement

--- solution5.php
+++ solution5.php
@@ -4,8 +4,8 @@
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 $highest = 0;
 $lowest = 0;
-$max = 10;
-for($n2 = 1; $n2 < 6000; $n2++) {
+$max = 20;
+for($n2 = 1; $n2 < 400000000; $n2++) {
 
     for ($n1=1; $n1 <= $max; $n1++) { 
         if($n2 % $n1 !== 0 ){
@@ -25,4 +25,4 @@ for($n2 = 1; $n2 < 6000; $n2++) {
   
 }
 
-echo $lowest;
+echo $highest;
