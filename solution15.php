3223ea2f2c6ce727fba9fb8eb4eb23f0ed5abb34
trentraynard <trentersr@gmail.com>
2019-07-26 09:27:56 -0700

    solution15 solved

--- solution15.php
+++ solution15.php
@@ -1,1 +1,21 @@
 <?php
+// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down 
+// How many such routes are there through a 20×20 grid?
+$n1 = 40;
+$n2 = 20;
+$n3 = $n1 - $n2;
+
+$fact1 = factorial($n1);
+$fact2 = factorial($n2);
+$fact3 = factorial($n3);
+
+echo $fact1 / ($fact2 * $fact3);
+
+function factorial($number)
+{
+    $factorial = 1;
+    for ($i = 1; $i <= $number; $i++) {
+        $factorial = $factorial * $i;
+    }
+    return $factorial;
+}
